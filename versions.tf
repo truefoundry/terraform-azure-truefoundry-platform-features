5cf968d84785c29d2c3e669df9b4c8454239c619
Harshit Luthra <love.coolsachin@gmail.com>
2024-11-04 02:31:06 +0530

    chore(terraform): update provider version constraints to allow newer versions

--- versions.tf
+++ versions.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.94.0"
+      version = ">= 3.94.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "2.47.0"
+      version = ">= 2.47.0"
     }
   }
 }
