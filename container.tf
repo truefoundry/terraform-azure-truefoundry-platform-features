a99a9901fd504a41048d5a49655c0126642b0410
Harshit Luthra <love.coolsachin@gmail.com>
2024-11-06 15:29:32 +0530

    fix(container.tf): correct attribute name to `https_traffic_only_enabled` for clarity and consistency
    chore(versions.tf): update azuread provider version to `>= 3.0.2` to ensure compatibility with latest features and improvements

--- container.tf
+++ container.tf
@@ -1,12 +1,11 @@
 resource "azurerm_storage_account" "truefoundry_platform_storage_account" {
-  count                    = var.platform_feature_enabled ? var.feature_blob_storage_enabled ? 1 : 0 : 0
-  name                     = local.storage_account_name
-  resource_group_name      = var.resource_group_name
-  location                 = var.location
-  account_tier             = var.blob_storage_account_account_tier
-  account_replication_type = var.blob_storage_account_replication_type
-
-  enable_https_traffic_only     = true
+  count                         = var.platform_feature_enabled ? var.feature_blob_storage_enabled ? 1 : 0 : 0
+  name                          = local.storage_account_name
+  resource_group_name           = var.resource_group_name
+  location                      = var.location
+  account_tier                  = var.blob_storage_account_account_tier
+  account_replication_type      = var.blob_storage_account_replication_type
+  https_traffic_only_enabled    = true
   access_tier                   = "Hot"
   min_tls_version               = "TLS1_2"
   public_network_access_enabled = true
