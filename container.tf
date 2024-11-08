5691724f4dfba0cbc590aba594d6034826452923
Harshit Luthra <love.coolsachin@gmail.com>
2024-11-08 05:50:30 +0530

    fix(container.tf): correct attribute name from https_traffic_only_enabled to enable_https_traffic_only for azurerm_storage_account resource

--- container.tf
+++ container.tf
@@ -5,7 +5,7 @@ resource "azurerm_storage_account" "truefoundry_platform_storage_account" {
   location                      = var.location
   account_tier                  = var.blob_storage_account_account_tier
   account_replication_type      = var.blob_storage_account_replication_type
-  https_traffic_only_enabled    = true
+  enable_https_traffic_only     = true
   access_tier                   = "Hot"
   min_tls_version               = "TLS1_2"
   public_network_access_enabled = true
