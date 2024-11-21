45d0d6191236340a90fe5f3e9b8b27f725f6b778
Harshit  Luthra <love.coolsachin@gmail.com>
2024-11-21 14:29:55 +0530

    fix(locals.tf): adjust container_registry_name substring to start from index 0 for consistency with other naming conventions (#11)

--- locals.tf
+++ locals.tf
@@ -2,7 +2,7 @@ locals {
   cluster_name               = replace(replace(var.cluster_name, "-", ""), "_", "")
   storage_account_name       = var.blob_storage_account_enable_override ? var.blob_storage_account_override_name : "${substr(local.cluster_name, 0, 22)}sa"
   container_name             = var.blob_container_enable_override ? var.blob_container_override_name : "${substr(var.cluster_name, 0, 62)}cn"
-  container_registry_name    = var.container_registry_enable_override ? var.container_registry_override_name : "${substr(local.cluster_name, 5, 47)}reg"
+  container_registry_name    = var.container_registry_enable_override ? var.container_registry_override_name : "${substr(local.cluster_name, 0, 47)}reg"
   azuread_application        = var.cluster_integration_azuread_application_enable_override ? var.cluster_integration_azuread_application_override_name : "${substr(var.cluster_name, 0, 117)}app"
   service_principal_password = "${local.cluster_name}sp"
   tags = merge(
